b6ecc4b73865848d670984983891d09d8c2bd0b1
jmikulsk <jmikulsk@wszib.edu.pl>
2020-02-29 12:13:21 +0100

    przed layautem

--- src/main/java/pl/edu/wszib/controller/ShoopController.java
+++ src/main/java/pl/edu/wszib/controller/ShoopController.java
@@ -1,6 +1,7 @@
 package pl.edu.wszib.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,15 @@ public class ShoopController {
     @GetMapping
     public String welcome(){
         return "welcome";
+    }
+    @GetMapping("shipping")
+    public String shipping(Model model){
+        model.addAttribute("shippingMethodName1", "Paczkomat");
+        model.addAttribute("shippingMethodName2", "Kurier - przedpłata");
+        model.addAttribute("shippingMethodName3", "Kurier - pobranie");
+        model.addAttribute("shippingMethodPrice1",10.5);
+        model.addAttribute("shippingMethodPrice2",15.00);
+        model.addAttribute("shippingMethodPrice3",10.20);
+        return "shipping";
     }
 }
