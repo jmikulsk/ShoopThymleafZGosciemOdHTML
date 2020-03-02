9a1366bceb4805317d9aea0aa553ca4e60cfa168
KUBA <jmikulsk92>
2020-03-02 19:55:22 +0100

    dodanie REST USERA

---
+++ src/main/java/pl/edu/wszib/controller/RestUserController.java
@@ -0,0 +1,31 @@
+package pl.edu.wszib.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.wszib.dao.UserDao;
+import pl.edu.wszib.domain.User;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin
+public class RestUserController {
+
+    @Autowired
+    private UserDao userDao;
+
+    @GetMapping("users")
+    public List<User> users(){
+        return  userDao.getUsers();
+    }
+
+    @DeleteMapping("users/remove/{id}")
+   public void removeUser(@PathVariable Long id){
+        userDao.removeUser(id);
+}}
+
+
+
+
